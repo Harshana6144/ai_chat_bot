64d15eea7c87515ffe3dc65902bac17ceddad63c
Harshana Prasad <harshana7096@gmail.com>
2025-02-06 22:25:26 +0530

    created claude api service

---
+++ lib/chat/data/claude_api_service.dart
@@ -0,0 +1,14 @@
+/*
+
+Service class to handle all Clude API stuff
+
+*/
+
+class ClaudeApiService{
+  //API Constants
+  static const String _baseUrl = 'https://api.anthropic.com/v1/messages';
+  static const String _apiVersion = '2023-06-01';
+  static const String _model = 'claude-3-opus-20240229';
+
+  //Strore the API
+}
